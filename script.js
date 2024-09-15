69833f15cc7b6a30939d4ce13aeafdba29a8d1ec
Daniel Casita <legendadtwo@gmail.com>
2024-09-15 17:35:05 -0500

    Logica dropdown

--- script.js
+++ script.js
@@ -5,7 +5,37 @@ const question4 = document.getElementById("question4");
 
 
 function Faq1(){
-    const answer = document.getElementById("answer");
+    const answer = document.getElementById("answer1");
+
+    if (answer.classList.contains("hidden")) {
+        answer.classList.remove("hidden");
+    } else {
+        answer.classList.add("hidden");
+    }
+}
+
+function Faq2(){
+    const answer = document.getElementById("answer2");
+
+    if (answer.classList.contains("hidden")) {
+        answer.classList.remove("hidden");
+    } else {
+        answer.classList.add("hidden");
+    }
+}
+
+function Faq3(){
+    const answer = document.getElementById("answer3");
+
+    if (answer.classList.contains("hidden")) {
+        answer.classList.remove("hidden");
+    } else {
+        answer.classList.add("hidden");
+    }
+}
+
+function Faq4(){
+    const answer = document.getElementById("answer4");
 
     if (answer.classList.contains("hidden")) {
         answer.classList.remove("hidden");
@@ -15,3 +45,6 @@ function Faq1(){
 }
 
 question1.addEventListener('click', Faq1);
+question2.addEventListener('click', Faq2);
+question3.addEventListener('click', Faq3);
+question4.addEventListener('click', Faq4);
